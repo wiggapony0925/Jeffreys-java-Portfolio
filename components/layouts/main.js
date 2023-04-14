63b0c82f144addf34ad72b71c0008e6033a53b29
wiggapony0925 <ninjeff06@gmail.com>
2023-04-13 23:52:30 -0400

    fixed main.js image

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,20 +10,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Jeffrey's homepage" />
         <meta name="author" content="Jeffrey Fernandez" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Jeffrey Fernandez" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:image" content="/images/card.png" />
+        <meta property="og:site_name" content="Jeffrey Fernandez" />
+        <meta name="og:title" content="Jeffrey Fernandez" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/images/card.png" />
         <title>Jeffrey Fernandez - Homepage</title>
       </Head>
 
