c383b88e82513c67a50fa425585e985d17b364af
wiggapony0925 <ninjeff06@gmail.com>
2023-07-18 17:57:23 -0400

    careerwise training pdf

--- pages/works.js
+++ pages/works.js
@@ -1,72 +1,118 @@
-import { Center, Container, Heading, SimpleGrid, Badge, Avatar,Flex, Box, Button, Text, HStack} from '@chakra-ui/react'
-import { FaGithub } from 'react-icons/fa'
-import NextLink from 'next/link'
-import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.png'
+import { Center, Container, Heading, SimpleGrid, Badge, Avatar, Flex, Box, Button, Text, HStack } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import NextLink from 'next/link';
+import Layout from '../components/layouts/article';
+import Section from '../components/section';
+import { WorkGridItem } from '../components/grid-item';
+import thumbWalknote from '../public/images/works/walknote_eyecatch.png';
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png';
+import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.png';
+import thumbCareerwise from '../public/images/works/careerwise.png';
 
 const Works = () => (
   <Layout title="Works">
     <Container>
-    <Center>
-      <Flex mt="35" align="center">
-        <Avatar src="/images/takuya.png" ml="3" mb="3" alignContent="center" />
-        <Box ml="3">
-          <Text fontWeight="bold">
-            Jeffrey Fernandez
-            <Badge ml="3" colorScheme="teal">
-              Active
-            </Badge>
-          </Text>
-          <Text fontSize="sm">Software Engineer</Text>
+      <Center>
+        <Flex mt="15" align="center">
+          <Avatar src="/images/takuya.png" ml="3" mb="3" alignContent="center" />
+          <Box ml="3">
+            <Text fontWeight="bold">
+              Jeffrey Fernandez
+              <Badge ml="3" colorScheme="teal">
+                Active
+              </Badge>
+            </Text>
+            <Text fontSize="sm">Software Engineer</Text>
+          </Box>
+        </Flex>
+      </Center>
+
+      <Section>
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Heading as="h3" fontSize={20} mt={10} mb={6} textAlign="center" variant="section-title">
+            Careerwise/Business Essentials
+          </Heading>
+
+          <SimpleGrid columns={[1, 1, 2]} gap={6}>
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              <WorkGridItem id="week_one" title="Week One" thumbnail={thumbCareerwise}>
+                This is the first week of the careerwise/business essentials program.
+              </WorkGridItem>
+            </motion.div>
+          </SimpleGrid>
+        </motion.div>
+      </Section>
+
+      <Section>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Heading as="h3" fontSize={20} mb={4} mt={20} textAlign="center" variant="section-title">
+            Works
+          </Heading>
+
+          <SimpleGrid columns={[1, 1, 2]} gap={6}>
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              <WorkGridItem id="virusSimulator" title="Python Virus Building Simulation" thumbnail={thumbInkdrop}>
+                Graphs the state of each individual in the building simulation.
+              </WorkGridItem>
+            </motion.div>
+
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              <WorkGridItem id="plotting" title="MERRA-2 NetCDF plotting" thumbnail={thumbWalknote}>
+                Plotting a Graph from MERRA-2 NetCDF Files using Python.
+              </WorkGridItem>
+            </motion.div>
+
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              <WorkGridItem id="MNIST" title="MNIST dataset with neural networks" thumbnail={thumbFourPainters}>
+                MNIST provides a baseline for testing image processing systems.
+              </WorkGridItem>
+            </motion.div>
+          </SimpleGrid>
+        </motion.div>
+      </Section>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Box align="center" my={4} display="flex" justifyContent="center">
+          <HStack alignItems="center" as={NextLink} href="https://github.com/wiggapony0925?tab=repositories" scroll={false}>
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.97 }}
+            >
+              <Button colorScheme="teal" leftIcon={<FaGithub />} size="lg">
+                My Projects
+              </Button>
+            </motion.div>
+          </HStack>
         </Box>
-      </Flex>
-    </Center>
-
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-        <WorkGridItem id="inkdrop" title="Python Virus Building Simulation" thumbnail={thumbInkdrop}>
-        Graphs the state of each individual in the building simulation
-          </WorkGridItem>
-        </Section>
-
-        <Section>
-        <WorkGridItem
-            id="walknote"
-            title="MERRA-2 NetCDF plotting"
-            thumbnail={thumbWalknote}
-          > Plotting a Graph from MERRA-2 NetCDF Files using Python
-            
-          </WorkGridItem>
-        </Section>
-
-        <Section delay={0.1}>
-        <WorkGridItem id="fourpainters" title="MNIST dataset with neural networks" thumbnail={thumbFourPainters}>
-          MNIST provides a baseline for testing image processing systems
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-      <Box align="center" my={4} display='flex' justifyContent="center">
-      <HStack 
-      alignItems={"center"}
-      as={NextLink}
-      href="https://github.com/wiggapony0925?tab=repositories"
-      croll={false}>
-        <Button colorScheme='teal' leftIcon={<FaGithub />}>
-          My Projects
-        </Button>
-      </HStack>
-      </Box>
-     </Container>
+      </motion.div>
+    </Container>
   </Layout>
-)
+);
 
-export default Works
-export { getServerSideProps } from '../components/chakra'
+export default Works;
+export { getServerSideProps } from '../components/chakra';
---
+++ pages/works/week_one.js
@@ -0,0 +1,91 @@
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  UnorderedList,
+  Heading,
+  Center
+} from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const WeekOne = () => (
+  <Layout title="Week One">
+    <Container>
+      <Title>
+        Excel Formulas, Professional Email Templates, and Resume Writing <Badge>Week One</Badge>
+      </Title>
+      <P>
+        During the first week, we focused on mastering Excel formulas, creating professional email templates, and developing effective resumes. We learned various techniques to efficiently manipulate data and automate calculations using Excel. Additionally, we explored best practices for designing email templates to communicate professionally in a business setting, and we gained insights into crafting compelling and well-structured resumes to showcase our skills and experiences.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Excel Formulas</Meta>
+          <span>
+            - Advanced functions like VLOOKUP and IF statements <br />
+            - Data manipulation using formulas and conditional formatting <br />
+            - Creating dynamic charts and graphs
+          </span>
+        </ListItem>
+        <ListItem>
+          <Meta>Professional Email Templates</Meta>
+          <span>
+            - Crafting effective subject lines and email greetings <br />
+            - Writing clear and concise email content <br />
+            - Incorporating proper email etiquette and professional tone
+          </span>
+        </ListItem>
+        <ListItem>
+          <Meta>Resume Writing</Meta>
+          <span>
+            - Structuring a compelling resume layout <br />
+            - Showcasing relevant skills, experiences, and achievements <br />
+            - Tailoring the resume to specific job requirements
+          </span>
+        </ListItem>
+      </List>
+
+      <Heading as="h4" fontSize={16} my={6}>
+        <Center>Media Coverage</Center>
+      </Heading>
+
+      <UnorderedList my={4}>
+        <ListItem>
+          <Link href="https://www.example.com">
+            <Badge mr={2}>Video</Badge>
+            Excel Formulas Mastery
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+
+        <ListItem>
+          <Link href="https://www.example.com">
+            <Badge mr={2}>Article</Badge>
+            Creating Professional Email Templates
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+
+        <ListItem>
+          <Link href="https://www.example.com">
+            <Badge mr={2}>Guide</Badge>
+            Effective Resume Writing Techniques
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+      </UnorderedList>
+
+      <WorkImage
+        src="/images/works/careerwise"
+        alt="Careerwise"
+      />
+    </Container>
+  </Layout>
+);
+
+export default WeekOne;
+export { getServerSideProps } from '../../components/chakra';
