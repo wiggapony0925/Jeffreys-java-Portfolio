239f9fa3d958b5eb5fd656391e0410f2374dfea9
JFMCaptialGroupLLC <capital.jfm@gmail.com>
2026-01-31 21:21:31 -0500

    chore: Add Node.js engine constraint and remove unused imports from `logo.js` and `purepay.js`.

--- pages/works/purepay.js
+++ pages/works/purepay.js
@@ -1,14 +1,12 @@
 import {
     Container,
     Badge,
-    Link,
     List,
     ListItem,
     Heading,
     Center,
     UnorderedList
 } from '@chakra-ui/react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Title, WorkImage, Meta } from '../../components/work';
 import P from '../../components/paragraph';
 import Layout from '../../components/layouts/article';
